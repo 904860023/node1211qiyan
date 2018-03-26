9a59dc7e7c6a5ae9ef0c50fcde37f5ef9dcfe110
dllo <904860023@qq.com>
2018-03-26 19:56:23 +0800

    【提交人】齐颜 【提交时间】2018年3月26日 【提交内容】命令箭头函数等

--- node/node.js
+++ node/node.js
@@ -1,2 +1,26 @@
 //cishi
 //haha
+console.log("hello node");
+{
+var a = 10;
+let b = 11;  //let有作用域 只在当前代码段
+    console.log(b);
+}
+console.log(a);
+// console.log(b);
+
+// var a=[];
+// for(var i=0;i<10;i++){
+//     a[i]=function(){
+//         console.log(i);
+//     };
+// }
+// a[6](); //10
+
+var a=[];
+for(let i=0;i<10;i++){
+    a[i]=function(){
+        console.log(i);
+    };
+}
+a[6]();    //6
